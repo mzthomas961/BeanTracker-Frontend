99211c19fcf0e79281041fe34f075859cf1046fb
“Aaron <github email address>
2021-03-09 14:52:49 -0500

    working on createOrder

--- src/index.js
+++ src/index.js
@@ -23,6 +23,20 @@ function getAllOrders(){
     })
 }
 
+function createOrder(order){
+    fetch(orderUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({order})
+    })
+        .then(response => console.log(response.json()))
+        //.then(newOrder => console.log(newOrder))
+
+}
+
 // Rendering Logic
 function getOneOrder(orders){
     orders.forEach(order => {
@@ -32,17 +46,15 @@ function getOneOrder(orders){
 
 function renderAllOrders(order){
     id = order.coffee_id
-    
     fetch(`${coffeeUrl}/${id}`)
     .then(r => r.json())
     .then(coffee => {
         cart.innerHTML += `<li id = ${order.id}>
-        <h1> Coffee: ${coffee.name} </h1>
+        <h2> Coffee: ${coffee.name} </h1>
+        <h2> Note: ${order.note} </h2>
+        <h4> Date/Time: ${order.date} </h2>
         </li>`
     })
-
-   
-    
 
 }
 function getOneCoffee(coffees) {
@@ -61,11 +73,11 @@ function renderAllCoffees(coffee) {
 function renderCoffeeObj(coffee){
     detail.querySelector("img.detail-image").src = coffee.image
     detail.querySelector("h2.name").textContent = coffee.name
-    // form.querySelector("textarea#comment").value = ramen.comment
     commentForm.dataset.id = coffee.id
 }
 
 // Event Handlers
+
 
 function handleMenuClick(e) {
     if (e.target.tagName === 'IMG') {
@@ -74,27 +86,26 @@ function handleMenuClick(e) {
         .then(coffee => renderCoffeeObj(coffee))
     }
 }
-function handleFormSubmission(e){
-    e.preventDefault()
-    const comment = e.target[0].value
-    fetch(`${coffeeUrl}/${commentForm.dataset.id}`,{
-        method: 'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(commentO)
+
+function handleFormSubmission() {
+    commentForm.addEventListener('submit', function(e) {
+        e.preventDefault()
+        let order = {
+            note:e.target[0].value,
+            date:e.target[1].value,
+            coffee_id:e.target.dataset.id,
+            user_id:1
+        }
+        createOrder(order)
     })
-    .then(response => response.json())
-    .then(comment => {
-        console.log(comment)})
 }
 
 // Event Listeners
 
 menu.addEventListener('click', handleMenuClick)
-commentForm.addEventListener('submit', handleFormSubmission)
 
 // Initialize
 
 getAllCoffees()
 getAllOrders()
+handleFormSubmission()
